89a03dca95b97d1220d35f6978e53671b6d46874
pixma38 <pixma38@gmail.com>
2014-04-04 08:46:59 +0530

    menu resource added

--- mclient/resource.h
+++ mclient/resource.h
@@ -6,16 +6,22 @@
 #define appTitle                        294
 #define IDI_ICON_APP                    295
 #define appErrorCommentMsg              296
+#define IDR_MENU1                       296
 #define wndRegFailErrMsg                297
 #define wndCreationFailedErrMsg         298
 #define wndErrorTitle                   299
+#define ID_Menu                         40001
+#define ID_FILE_EXIT                    40002
+#define ID_MAILS_NEWMAIL                40003
+#define ID_COMPOSE_NEW_MAIL             40004
+#define ID_HELP_ABOUT                   40005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        296
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        297
+#define _APS_NEXT_COMMAND_VALUE         40006
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
