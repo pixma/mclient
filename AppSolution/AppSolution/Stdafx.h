0530ce484897744396a7743829473b48fc387c20
pixma38@gmail.com <pixma38@gmail.ccom>
2015-03-16 22:37:41 +0530

    Normal Push with initial Resource file content.

--- AppSolution/AppSolution/Stdafx.h
+++ AppSolution/AppSolution/Stdafx.h
@@ -4,6 +4,9 @@
 
 #include <Windows.h>
 #include<CommCtrl.h>
+#include <string.h>
+
+#include "res_file\resource\resource.h"
 
 #pragma comment(lib,"comctl32.lib")
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
---
+++ AppSolution/AppSolution/res_file/resource/resource.h
@@ -0,0 +1,5 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDS_APPTITLE                            40000
